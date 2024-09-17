5b75bed611501c4bac9aaeea61f43fb9d882b537
snwdaaa <kkj4818@naver.com>
2024-09-17 20:27:04 +0900

    - Cursor Manager 추가

---
+++ Assets/Scripts/Player/Movement/CursorManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 마우스 커서 관리 스크립트
+/// </summary>
+public class CursorManager : MonoBehaviour
+{
+    private void Start()
+    {
+        HideCursor();
+    }
+
+    private void ShowCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
