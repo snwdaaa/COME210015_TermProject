82d924680fa22bbd051c5af529e6515fa14a88af
snwdaaa <kkj4818@naver.com>
2024-09-26 18:00:25 +0900

    미완성 업데이트

---
+++ Assets/Scripts/Player/Status/PlayerStamina.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStamina : MonoBehaviour
+{
+    // 달리기, 점프할 때 스태미너 사용
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
