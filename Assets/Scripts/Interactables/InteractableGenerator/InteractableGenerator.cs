d7843f6140099ef04c3fb94fd55a60a1f782a0a0
snwdaaa <kkj4818@naver.com>
2024-09-29 17:51:48 +0900

    미완성 업데이트
    - Interactable Generator 스크립트 추가 (미완성)

---
+++ Assets/Scripts/Interactables/InteractableGenerator/InteractableGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableGenerator : MonoBehaviour
+{
+    private float progress = 0f; // ÁøÇàµµ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
