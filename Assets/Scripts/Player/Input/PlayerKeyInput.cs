4ba04e719ab4f4e6d7d62bed17a0b024100f13a6
snwdaaa <kkj4818@naver.com>
2024-10-03 01:43:45 +0900

    - 업데이트
    PostProcessing 추가

--- Assets/Scripts/Player/Input/PlayerKeyInput.cs
+++ Assets/Scripts/Player/Input/PlayerKeyInput.cs
@@ -15,22 +15,16 @@ public class PlayerKeyInput : MonoBehaviour
     // 플레이어 입력 여부 프로퍼티
     // 키보드
     public Vector2 moveInput { get; private set; }
-    public bool keyPressed_Sprint { get; private set; }
     public bool keyPressed_Crouch { get; private set; }
     public bool keyPressed_Fire { get; private set; }
 
-    // FixedUpdate에서 GetButtonDown을 검사하는 경우 키가 무시되는 경우가 있음
-    // 이 스크립트에서 키가 감지되면 true로 바꾸고, 다른 스크립트에서 처리 후 false로 바꿔주는 방식을 사용
-    public bool keyPressed_Jump { get; set; }
     public bool keyPressed_Use { get; set; }
 
     private void Update()
     {
         DetectKeyInput_MoveInput();
-        DetectKeyInput_Sprint();
         DetectKeyInput_Crouch();
         DetectKeyInput_Use();
-        DetectKeyInput_Jump();
     }
 
     /// <summary>
@@ -44,19 +38,6 @@ public class PlayerKeyInput : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
 
         moveInput = new Vector2(horizontalInput, verticalInput).normalized;
-    }
-
-    private void DetectKeyInput_Sprint()
-    {
-        keyPressed_Sprint = Input.GetButton("Sprint");
-    }
-
-    private void DetectKeyInput_Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            keyPressed_Jump = true;
-        }
     }
 
     private void DetectKeyInput_Crouch()
